e99e526de05b67f361978b97ccb8b2f9d6d0abdc
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-02-21 18:41:22 +0800

    add promo to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { injectIntl, InjectedIntlProps } from "react-intl";
 import { Layout } from "../components/Layout";
 import { withIntl } from "../i18n/withIntl";
 import { AngledHeader } from "src/components/Page";
+import { PromoSection } from "src/components/Promotion";
 
 export interface IndexPageProps {
   pageContext: {
@@ -12,10 +13,31 @@ export interface IndexPageProps {
 
 type Props = IndexPageProps & InjectedIntlProps;
 
+const promo = {
+  frontmatter: {
+    title: "Fake Promotion",
+    subtitle: "February promotion",
+    buttonText: "50% Off",
+    dateStart: "2019-02-01",
+    dateEnd: "2019-02-28",
+    image: null,
+    kinds: ["white"],
+    wineries: [],
+    wines: [],
+  },
+  fields: {
+    slug: "/",
+    lang: "en",
+  },
+  htmlAst: null,
+  html: "<p>body</p>",
+};
+
 const IndexPage: React.SFC<Props> = ({ pageContext, intl }) => {
   return (
     <Layout>
       <AngledHeader title={"Event"} subtitle="Special Event" />
+      <PromoSection promo={promo} />
     </Layout>
   );
 };
