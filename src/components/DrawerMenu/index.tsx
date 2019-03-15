11fc14596a5e18d91c3d22ff389845c79ae5f3b6
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-03-15 14:30:58 +0800

    add useKeyDown

--- src/components/DrawerMenu/index.tsx
+++ src/components/DrawerMenu/index.tsx
@@ -2,7 +2,10 @@ import * as React from "react";
 import { MenuButton } from "./MenuButton";
 import { CloseButton } from "./CloseButton";
 import { styled, Flex } from "primithemes";
-import { useTransition, animated } from "react-spring";
+import { useDrawer } from "./drawerHooks";
+import { animated } from "react-spring";
+
+const width = "300px";
 
 const DrawerWrapper = styled.div`
   z-index: 1400;
@@ -38,7 +41,7 @@ const DrawerContentWrapper = styled(Flex)`
   height: 100vh;
   position: relative;
   overflow-y: auto;
-  width: 300px;
+  width: ${width};
 `;
 
 interface DrawerMenuProps {
@@ -47,72 +50,32 @@ interface DrawerMenuProps {
   navItems: { to: string; label: string }[];
 }
 
-const useShowMenu = () => {
-  const [show, set] = React.useState(false);
-  const open = () => {
-    set(true);
-  };
-
-  const close = () => {
-    set(false);
-  };
-
-  return { show, open, close };
-};
-
-const useDisableBodyScroll = (show: boolean) => {
-  React.useEffect(() => {
-    document.body.style.overflowY = show ? "hidden" : "scroll";
-  }, [show]);
-};
-
-const useDrawer = (width: number) => {
-  const { show, open, close } = useShowMenu();
-  useDisableBodyScroll(show);
-
-  const drawer = useTransition(show, null, {
-    from: { transform: `translate3d(${width}px,0,0)` },
-    enter: { transform: `translate3d(0,0,0)` },
-    leave: { transform: `translate3d(${width}px,0,0)` },
-  });
-
-  const overlay = useTransition(show, null, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-  });
-
-  return { show, open, close, drawer, overlay };
-};
-
 const DrawerMenu: React.SFC<DrawerMenuProps> = ({ logo, title, navItems }) => {
-  const { open, close, drawer, overlay } = useDrawer(300);
+  const { open, close, drawer, overlay } = useDrawer(width);
 
   return (
     <DrawerWrapper>
       <MenuButton onClick={open} />
-      <>
-        {overlay.map(
-          ({ item, key, props }) =>
-            item && <DrawerOverlay key={key} style={props} onClick={close} />
-        )}
-        {drawer.map(
-          ({ item, key, props }) =>
-            item && (
-              <Dmenu key={key} style={props}>
-                <DrawerContent>
-                  <DrawerContentWrapper
-                    flexDirection="column"
-                    bg="background.light"
-                    p={3}
-                  >
-                    <CloseButton onClick={close} />
-                  </DrawerContentWrapper>
-                </DrawerContent>
-              </Dmenu>
-            )
-        )}
-      </>
+      {overlay.map(
+        ({ item, key, props }) =>
+          item && <DrawerOverlay key={key} style={props} onClick={close} />
+      )}
+      {drawer.map(
+        ({ item, key, props }) =>
+          item && (
+            <Dmenu key={key} style={props}>
+              <DrawerContent>
+                <DrawerContentWrapper
+                  flexDirection="column"
+                  bg="background.light"
+                  p={3}
+                >
+                  <CloseButton onClick={close} />
+                </DrawerContentWrapper>
+              </DrawerContent>
+            </Dmenu>
+          )
+      )}
     </DrawerWrapper>
   );
 };
